895f088b78f1a82b88916f3308aea4bb30048ca1
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-16 16:30:03 +0200

    First php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    // $password = 'Boolean';
+
+    $password = $_GET['password'];
+
+    if ($password == 'Boolean') {
+            echo "<h1 style='color:green'>Password Corretta</h1>";
+    } else {
+            echo "<h1 style='color:red'>Password Non Corretta</h1>";
+    }
+?>
